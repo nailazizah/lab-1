c49905fa23948e3cacb75ace19f251585168af3d
Naila Azizah <naila.azizah@ui.ac.id>
2022-09-15 13:46:41 +0700

    Lab 2: Implement data delivery

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('wishlist/', include('wishlist.urls')),
+    path('', include('wishlist.urls')),
 ]
